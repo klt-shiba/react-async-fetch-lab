ab5c5b4e41ef9840bf6071a329b8b5e4505d2495
Colin Tseng <klt.aus@yahoo.com>
2021-05-24 19:08:28 +1000

    Completed assignment

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    constructor() {
+        super()
+        
+        this.state = {
+            peopleInSpace: []
+        }
+    }
+    componentDidMount() {
+        fetch("http://api.open-notify.org/astros.json")
+        .then(response => response.json())
+        .then(obj => {this.setState({
+            peopleInSpace: obj.people
+            })
+        })
+    }
+    render() {
+        return (
+            <div>
+                {this.state.peopleInSpace.map(el => el.name)}
+            </div>
+        )
+    }
+}
